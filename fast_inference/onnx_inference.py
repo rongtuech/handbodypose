1970b4e0159e47b948be9a3c0e64072256ba59f1
Ngo Trung <trungngotan94@gmail.com>
2021-06-15 22:44:32 +0900

    update ONNX/TensorRT inference

--- fast_inference/onnx_inference.py
+++ fast_inference/onnx_inference.py
@@ -8,13 +8,19 @@ class PoseDetectionONNX:
         self.session_option.graph_optimization_level = onnxruntime.GraphOptimizationLevel.ORT_ENABLE_ALL
         self.ort_session = onnxruntime.InferenceSession(onnx_path, sess_options=self.session_option)
         self.ort_session.set_providers(['CUDAExecutionProvider'])
+        self.io_binding = self.ort_session.io_binding()
+        self.io_binding.bind_output(self.ort_session.get_outputs()[0].name)
+        # self.io_binding.bind_output(self.ort_session.get_outputs()[1].name)
+        print(onnxruntime.get_device())
 
     def __call__(self, input):
         # expand batch size
         input = np.expand_dims(input,axis = 0)
-        # init input
-        ort_inputs = {self.ort_session.get_inputs()[0].name: input}
-        # inference
+        ortvalue = onnxruntime.OrtValue.ortvalue_from_numpy(input, 'cuda', 0)
+        #
+        # # init input
+        ort_inputs = {self.ort_session.get_inputs()[0].name: ortvalue}
+        # # inference
         pafs, heatmaps = self.ort_session.run(None, ort_inputs)
 
         return pafs, heatmaps
